9241f4f8e6e6b9de6e9fb54fd1c1e62db1915b19
Matheus Romero <matheus.hoshigaki@hotmail.com>
2021-06-04 21:43:45 -0300

    Feito a soma do dois números.

--- src/App.js
+++ src/App.js
@@ -6,15 +6,7 @@ export default class App
   extends React.Component {
   render() {
     return (<div>
-      <Secao
-        titulo="Secao 1"
-        descricao="Testando descrição 1."
-      />
-
-      <Secao
-        titulo="Secao 2"
-        descricao="Testando descrição 2."
-      />
+      <Secao />
     </div>)
   }
 }
--- src/Secao.js
+++ src/Secao.js
@@ -1,19 +1,44 @@
 import React from 'react';
-
+import "./secao.css"
 export default class Secao
     extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { num1: 0, num2: 0, result: 0 }
+    }
+    handleInput1(e) {
+        const value = e.target.value;
+        this.setState({ num1: value })
+    }
+    handleInput2(e) {
+        const value = e.target.value;
+        this.setState({ num2: value })
+    }
+    sum() {
+        this.setState({ result: Number.parseFloat(this.state.num1) + Number.parseFloat(this.state.num2) })
+    }
 
     render() {
-
-        const { titulo, descricao }
-            = this.props;
-
         return (
-            <div>
-                <h1>{titulo}</h1>
-
-                <h3>{descricao}</h3>
+            <div style={{
+                display: 'flex',
+                flex: '1',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                gap: '15px'
+            }}>
+                <p>{process.env.REACT_APP_TESTE}</p>
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <input onChange={e => this.handleInput1(e)} type="numeric" placeholder="Valor 1" />
+                    <input onChange={e => this.handleInput2(e)} type="numeric" placeholder="Valor 2" />
+                </div>
+                <button className="button" onClick={() => this.sum()} />
+                <p>{this.state.result}</p>
             </div>
+
+
         );
     }
 }
